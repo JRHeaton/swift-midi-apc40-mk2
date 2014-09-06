6d891b05862bf548b55ff77a47e70c9d1b6a9f2a
John Heaton <jheaton.dev@gmail.com>
2014-09-05 21:35:37 -0400

    Initial Commit

---
+++ bloopy/bloopy.h
@@ -0,0 +1,19 @@
+//
+//  bloopy.h
+//  bloopy
+//
+//  Created by John Heaton on 9/5/14.
+//  Copyright (c) 2014 John Heaton. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for bloopy.
+FOUNDATION_EXPORT double bloopyVersionNumber;
+
+//! Project version string for bloopy.
+FOUNDATION_EXPORT const unsigned char bloopyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <bloopy/PublicHeader.h>
+
+
